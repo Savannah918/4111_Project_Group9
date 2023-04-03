2b37b389784f301cdcb4a821dadd71a4c963f42b
BirdyLiu6471023 <sl5247@columbia.edu>
2023-04-03 16:30:03 -0400

    change username

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #     DATABASEURI = "postgresql://zy2431:123123@34.73.36.248/project1"
 #
 # Modify these with your own credentials you received from TA!
-DATABASE_USERNAME = "xt2267"
+DATABASE_USERNAME = "xt2276"
 DATABASE_PASSWRD = "2097"
 DATABASE_HOST = "34.148.107.47" # change to 34.28.53.86 if you used database 2 for part 2
 DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/project1"
